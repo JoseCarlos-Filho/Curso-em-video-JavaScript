dae02cb8c377f175f3af7f675b7533673cb46c79
José Carlos <etijcvivi@gmail.com>
2022-04-18 20:11:43 -0300

    Atualização dos exercício de JavaScript

---
+++ modulo e/exercicio 1/JS.js
@@ -0,0 +1,24 @@
+
+function contar() {
+
+    let first = document.querySelector(".firstN").value;
+    let second = document.querySelector(".firstS").value;
+    const passo = document.querySelector(".firstP").value;
+
+    // console.log(first);
+    // console.log(second);
+    // console.log(passo);
+
+    for (var i = first; i <= second; i=i+passo) {
+
+        res.innerHTML = `${i}`;
+        res.innerHTML = `1F449`;
+
+    }
+
+    res.innerHTML = `1F3C1`;
+
+
+
+
+}
