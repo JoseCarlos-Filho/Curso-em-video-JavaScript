df5efb91333acad5f8247b9d8f0f8840cb9103dd
José Carlos <etijcvivi@gmail.com>
2022-04-24 16:49:37 -0300

    Atualização de exercicios

--- modulo e/exercicio 2/JS.js
+++ modulo e/exercicio 2/JS.js
@@ -1,2 +1,18 @@
 
+function contar() {
+    let ini = document.querySelector('.enterNumber1').value;
+    let fim = document.querySelector('.enterNumber2').value;
+    let passo  = document.querySelector('.enterPasso').value;
+    let res = document.getElementById('res');
 
+    if (ini == 0 || fim == 0 || passo == 0) {
+        alert('[ERRO!] Faltando informações!')
+    }
+    else {
+        res.innerHTML = 'Contando: '
+
+       for(let i = ini; i <= fim; i += passo) {
+           res.innerHTML += `${i} \u{1F449}`
+       }
+    }
+}
