de85cc696adb177c8d44d0f868bcd9acb3c72801
José Carlos <etijcvivi@gmail.com>
2022-05-04 21:57:25 -0300

    Adicionando Modulo F Funções

--- modulo f/exercicio 2/JS.js
+++ modulo f/exercicio 2/JS.js
@@ -1,31 +1,39 @@
+let num = Number(document.querySelector('.Numero').value);
+let tab = document.getElementById('seltab');
+let array = [];
+let res = document.querySelector('div#res');
+
 function adicionar() {
 
-    let num = Number(document.querySelector('.Numero').value);
-    let array = [];
-    let tab = document.getElementById('seltab');
 
     if (num == '') {
         alert(`Por favor, digite um número!`);
 
     } else {
         tab.innerHTML = '';
-        array = num;
-
-        let item = document.createElement('option');
-        item.text = `Valor ${num} Adicionado`
-    }
-
-    if (num == 0) {
-        alert(`Por favor, digite um número!`);
-    }
-    else {
-        tab.innerHTML = '';
-        for (let i = 1; i <= 10; i++) {
-            let item = document.createElement('option'); // cria o elemento OPTION da tag <select></select>
-            item.text = `${num} x ${i} = ${num*i}`; // imprime as informações com a multiplicação na tabela
-            item.value = `tab${i}`; // seleciona o valor da tabela 
-            tab.appendChild(item); // adiciona os elementos de forma sequencial na tabela
+        for (let i = 1; i <= num; i++) {
+            let item = document.createElement('option');
+            item.text = `Valor ${num} Adicionado`
+            tab.appendChild(item);
+            array = num;
         }
+    }
+
+    function finalizar() {
 
     }
+
+    // if (num == 0) {
+    //     alert(`Por favor, digite um número!`);
+    // }
+    // else {
+    //     tab.innerHTML = '';
+    //     for (let i = 1; i <= 10; i++) {
+    //         let item = document.createElement('option'); // cria o elemento OPTION da tag <select></select>
+    //         item.text = `${num} x ${i} = ${num*i}`; // imprime as informações com a multiplicação na tabela
+    //         item.value = `tab${i}`; // seleciona o valor da tabela 
+    //         tab.appendChild(item); // adiciona os elementos de forma sequencial na tabela
+    //     }
+
+    // }
 }
