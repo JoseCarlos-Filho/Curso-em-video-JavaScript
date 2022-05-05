27c274eeed6683de27ab20eb678b2c9cae85ae22
José Carlos <etijcvivi@gmail.com>
2022-05-05 20:24:59 -0300

    Desafio JavaScript criando funções e manipulando Array

--- modulo f/exercicio 2/JS.js
+++ modulo f/exercicio 2/JS.js
@@ -1,39 +1,46 @@
-let num = Number(document.querySelector('.Numero').value);
-let tab = document.getElementById('seltab');
-let array = [];
+let num = document.querySelector('.Numero');
+let tabela = document.getElementById('seltab');
+let lista = [];
 let res = document.querySelector('div#res');
+
+// função para verificar se é numero ou não
+function eNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
+
+// função se está na lista
+function estaNaLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 function adicionar() {
 
-
-    if (num == '') {
-        alert(`Por favor, digite um número!`);
-
+    if (eNumero(num.value) && !estaNaLista(num.value, lista)) {
+        // alert('Tudo OK')
+        lista.push(Number(num.value));
+        let item = document.createElement('option'); // elemento adicinado de forma dinâmica
+        item.text = `Valor ${num.value} adicionado`;
+        tabela.appendChild(item);
     } else {
-        tab.innerHTML = '';
-        for (let i = 1; i <= num; i++) {
-            let item = document.createElement('option');
-            item.text = `Valor ${num} Adicionado`
-            tab.appendChild(item);
-            array = num;
-        }
+        alert('Valor inválido ou já encontrado na lista.')
+        // tab.innerHTML = '';
+        // for (let i = 1; i <= num; i++) {
+        //     let item = document.createElement('option');
+        //     item.text = `Valor ${num} Adicionado`
+        //     tab.appendChild(item);
+        //     array = num;
+        // }
     }
-
-    function finalizar() {
-
-    }
-
-    // if (num == 0) {
-    //     alert(`Por favor, digite um número!`);
-    // }
-    // else {
-    //     tab.innerHTML = '';
-    //     for (let i = 1; i <= 10; i++) {
-    //         let item = document.createElement('option'); // cria o elemento OPTION da tag <select></select>
-    //         item.text = `${num} x ${i} = ${num*i}`; // imprime as informações com a multiplicação na tabela
-    //         item.value = `tab${i}`; // seleciona o valor da tabela 
-    //         tab.appendChild(item); // adiciona os elementos de forma sequencial na tabela
-    //     }
-
-    // }
+    num.value = ''
+    num.facus()
 }
