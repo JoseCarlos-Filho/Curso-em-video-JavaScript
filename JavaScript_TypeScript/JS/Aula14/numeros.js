4b460953791e3c34563be09fc36b9bd668092c3b
José Carlos <etijcvivi@gmail.com>
2022-07-05 22:21:07 -0300

    Curso Otávio Miranda JavaScript-TypeScript básico avançado.

---
+++ JavaScript_TypeScript/JS/Aula14/numeros.js
@@ -0,0 +1,13 @@
+let num1 = 1; // number
+let num2 = 2.5; // Number
+
+// transformand number em string
+// nesta operação o numero num1 se torna uma string
+// concatenando seu valor com num2
+console.log(num1.toString() + num2);
+console.log(typeof num1);
+
+// Defini o numero de casas decimais a serem exibidas
+let numero1 = 10.5789551255547; //number
+console.log(numero1.toFixed(2));
+console.log(numero1.toFixed(4));
