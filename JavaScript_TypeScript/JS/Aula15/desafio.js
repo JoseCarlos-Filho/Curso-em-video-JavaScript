32cda3986c1bf3b00c09694d0e43f268ddbffd72
José Carlos <etijcvivi@gmail.com>
2022-07-19 22:57:27 -0300

    Curso Otávio Miranda JavaScript - TypeScript

--- JavaScript_TypeScript/JS/Aula15/desafio.js
+++ JavaScript_TypeScript/JS/Aula15/desafio.js
@@ -1,7 +1,7 @@
 let numero = prompt('Digite o numero a ser exibido :');
 document.body.innerHTML += `Seu número é: ${numero} <br />`;
 document.body.innerHTML += `Raiz quadrada é : ${Math.sqrt(numero)} <br />`;
-document.body.innerHTML += `${numero} é inteiro : ${Number.isInteger(num1)} <br />`;
+document.body.innerHTML += `${numero} é inteiro : ${Number.isInteger(numero)} <br />`;
 document.body.innerHTML += `${numero} é um NaN : ${Number.isNaN(numero)} <br />`;
 document.body.innerHTML += `Arredondando para baixo : ${Match.floor(numero)} <br />`;
 document.body.innerHTML += `Arredondando para cima : ${Match.ceil(numero)} <br />`;
