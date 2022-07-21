89574afb54439f973d7e8251ea38a5ea23178039
José Carlos <etijcvivi@gmail.com>
2022-07-20 21:57:22 -0300

    Curso Otávio Miranda JavaScript - TypeScript

--- JavaScript_TypeScript/JS/Aula15/desafio.js
+++ JavaScript_TypeScript/JS/Aula15/desafio.js
@@ -1,8 +1,8 @@
 let numero = Number(prompt('Digite o numero a ser exibido :'));
-document.body.innerHTML += `Seu número é: ${numero} <br />`;
-document.body.innerHTML += `Raiz quadrada é : ${Math.sqrt(numero)} <br />`;
-document.body.innerHTML += `${numero} é inteiro : ${Number.isInteger(numero)} <br />`;
-document.body.innerHTML += `${numero} é um NaN : ${Number.isNaN(numero)} <br />`;
-document.body.innerHTML += `Arredondando para baixo : ${Match.floor(numero)} <br />`;
-document.body.innerHTML += `Arredondando para cima : ${Match.ceil(numero)} <br />`;
-document.body.innerHTML += `Com duas casas decimais : ${numero.toFixed(2)} <br />`;
+document.body.innerHTML += `<p>Seu número é: ${numero} </p>`;
+document.body.innerHTML += `<p>Raiz quadrada é : ${Math.sqrt(numero)} </p>`;
+document.body.innerHTML += `<p>${numero} é inteiro : ${Number.isInteger(numero)} </p>`;
+document.body.innerHTML += `<p>${numero} é um NaN : ${Number.isNaN(numero)} </p>`;
+document.body.innerHTML += `<p>Arredondando para baixo : ${Math.floor(numero)} </p>`;
+document.body.innerHTML += `<p>Arredondando para cima : ${Math.ceil(numero)} </p>`;
+document.body.innerHTML += `<p>Com duas casas decimais : ${numero.toFixed(2)} </p>`;
