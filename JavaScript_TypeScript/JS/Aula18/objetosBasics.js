f38ab4df1543013dc9fb6047845e1ad07ba6fb39
José Carlos <etijcvivi@gmail.com>
2022-07-25 21:51:13 -0300

    Curso Otávio Miranda JavaScript - TypeScript

---
+++ JavaScript_TypeScript/JS/Aula18/objetosBasics.js
@@ -0,0 +1,7 @@
+// const array = [1,2,3];
+let array = [1,2,3];
+array.push(4);
+array[0] = 'José';
+// array = 'Outra'; este comando não pode ser utilizado quando a variavel é uma constante
+array = 'Opa';
+console.log(array);
