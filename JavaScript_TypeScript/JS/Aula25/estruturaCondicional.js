393c301f9e60a07284e9cae1f3bd649f26846013
José Carlos <etijcvivi@gmail.com>
2022-07-31 22:34:11 -0300

    Curso Otávio Miranda JavaScript - TypeScript Estrutura condicional

---
+++ JavaScript_TypeScript/JS/Aula25/estruturaCondicional.js
@@ -0,0 +1,9 @@
+// Utilizando numeros e avaliando comportamento. Estrutura Condicional
+
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+    console.log('Sim, o número esta entre 0 e 5.');
+} else {
+    console.log('o numero NÃO esta entre 0 e 5');
+}
