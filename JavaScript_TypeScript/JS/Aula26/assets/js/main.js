5730512f45af768fdb43be1d56f7c3f7c918b837
José Carlos <etijcvivi@gmail.com>
2022-08-02 23:07:07 -0300

    JavaScript TypeScript - Luiz Otavio - Modulo HTML e CSS: Testes

--- JavaScript_TypeScript/JS/Aula26/assets/js/main.js
+++ JavaScript_TypeScript/JS/Aula26/assets/js/main.js
@@ -1,10 +1,19 @@
 function telaPrincipal(){
-    const formulario = document.querySelector('.formulario');
+    const formulario = document.querySelector('.form-input');
     const resultado = document.querySelector('.result');
 
     function eventoForm (evento) {
         evento.preventDefault();
-        
+
+        const peso = formulario.querySelector('#input-peso');
+        const altura = formulario.querySelector('#input-altura');
+        console.log(peso.value);
+        console.log(altura.value);
+
     }
 
+    formulario.addEventListener('submit', eventoForm);
+
 }
+
+telaPrincipal();
