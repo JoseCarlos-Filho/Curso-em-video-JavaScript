3a11cd3b6894ca5282a45bf93dfa927e1c82e812
José Carlos <etijcvivi@gmail.com>
2022-08-03 22:48:47 -0300

    JavaScript TypeScript - Luiz Otavio - Modulo HTML e CSS: Calculo IMC

--- JavaScript_TypeScript/JS/Aula26/assets/js/main.js
+++ JavaScript_TypeScript/JS/Aula26/assets/js/main.js
@@ -15,31 +15,47 @@ function telaPrincipal(){
         console.log(peso);
         console.log(altura);
 
-        // Calculo do IMC
-        let IMC = (peso / (altura * altura)).toFixed(1);
-        // vizualização do resultado
-        console.log(IMC);
-
         // verifica se o usuário digitou ou não um peso válido
         // E verifica se o usuário digitou uma altura válido
         if(peso === "" || isNaN(peso)) {
             resultado.innerHTML = `<span>Digite um valor de peso válido!!!</span>`
         } else if (altura === "" || isNaN(altura)) {
             resultado.innerHTML = `<span>Digite um valor de altura válido!!!</span>`
-        } else if (peso === "" || altura ==="") {
-            resultado.innerHTML = `<span>Preencha os campos adequadamente!!!</span>`
-        }
+        } else {
 
-        
-        // if (IMC > 18.5) {
-        //     resultado.innerHTML = `
-        //         <span>Abaixo do peso</span>
-        //     `;
-        // } else if (IMC >= 18.5 && IMC < 25) {
-        //     resultado.innerHTML = `
-        //         <span>Peso normal</span>
-        //     `;
-        // }
+            // Calculo do IMC
+            let IMC = (peso / (altura * altura)).toFixed(1);
+            // vizualização do resultado
+            console.log(IMC);
+
+            // verificação do IMC calculado
+            if (IMC < 18.5) {
+                resultado.innerHTML = `
+                    <span>Abaixo do peso</span>
+                `;
+            } else if (IMC >= 18.5 && IMC < 25) {
+                resultado.innerHTML = `
+                    <span>Peso normal</span>
+                `;
+            }  else if (IMC >= 25 && IMC < 30) {
+                resultado.innerHTML = `
+                    <span>Sobrepeso</span>
+                `;
+            }  else if (IMC >= 30 && IMC < 35) {
+                resultado.innerHTML = `
+                    <span>Obesidade grau 1</span>
+                `;
+            }  else if (IMC >= 35 && IMC < 40) {
+                resultado.innerHTML = `
+                    <span>Obesidade grau 2</span>
+                `;
+            }  else if (IMC >= 40 && IMC ) {
+                resultado.innerHTML = `
+                    <span>Obesidade grau 3</span>
+                `;
+            } 
+
+        }
 
     }
 
