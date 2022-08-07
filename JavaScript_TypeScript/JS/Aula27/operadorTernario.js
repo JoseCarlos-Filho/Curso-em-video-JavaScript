a6ffd0ebc26a413c967931f594a6a0f37c9e96f8
José Carlos <etijcvivi@gmail.com>
2022-08-07 14:23:45 -0300

    JavaScript TypeScript - Luiz Otavio - objeto date

--- JavaScript_TypeScript/JS/Aula27/operadorTernario.js
+++ JavaScript_TypeScript/JS/Aula27/operadorTernario.js
@@ -3,6 +3,8 @@
 // const umDia = 60 * 60 * 24 * 1000; // calculo de tempo em milisegundos
 // const data = new Date(0 + tresHoras + umDia); // 01/01/1970 timestamp unix ou época unix
 
-const data = new Date();
+//const data = new Date(2019, 3, 20, 15, 14, 27); // ano, mes, dia, hora, minuto, segundo, milésimosesegundo.
+
+const data = new Date('2019-04-20 20:20:59'); // passando parametro de data por string
 console.log(data.toString());
 
