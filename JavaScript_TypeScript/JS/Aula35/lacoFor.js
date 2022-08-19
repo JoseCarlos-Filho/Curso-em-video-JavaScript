bef13dd7733fa3806cecfb13523d6d89c4821876
José Carlos <etijcvivi@gmail.com>
2022-08-18 22:08:42 -0300

    JavaScript e TypeScript Luiz Otavio - teste laço For

--- JavaScript_TypeScript/JS/Aula35/lacoFor.js
+++ JavaScript_TypeScript/JS/Aula35/lacoFor.js
@@ -1,17 +1,33 @@
 // Array de Objetos
 const elementos = [
     {tag: 'p', texto: 'Frase 1'},
+    {tag: 'h2',  texto: 'é nóis viado'},
     {tag: 'div', texto: 'Frase 2'},
     {tag: 'footer', texto: 'Frase 3'},
     {tag: 'section', texto: 'Frase 4'},
 ];
 
-const containerPage = document.querySelector('.container');
+// Monta elementos do Array de Objeto.
+function montarElementos (elementos) {
+    const containerPage = document.querySelector('.container');
+    
+     for (let index = 0; index < elementos.length; index++) {
+        const tag = document.createElement(elementos[index].tag);
+        tag.innerText = elementos[index].texto;
+        containerPage.appendChild(tag);
+        
+     }
+}
 
-for (let index = 0; index <= elementos.length; index++) {
-    containerPage.innerHTML = document.appendChild = elementos[index].tag,
-    containerPage.innerHTML = document.appendChild = elementos[index].texto;
+// Adiciona novo elemento no Array de Objetos.
+function novoElemento (tag, texto) {
+    elementos.push({tag, texto});
+    console.log(elementos);
+    montarElementos(elementos);
 }
 
 
+// montarElementos(elementos);
+setTimeout(novoElemento('h1', 'numero'), 5000);
+setTimeout(() =>  console.log('5 segundos'), 5000);
 
